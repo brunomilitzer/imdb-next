245a9e8c61c4ad5d07009e18862655c463034751
Bruno Militzer <brunomilitzer.work@gmail.com>
2025-02-26 20:39:20 +0000

    Added Navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/Header";
 import Providers from "@/app/Providers";
+import Navbar from "@/components/navbar/Navbar";
 
 const geistSans = Geist( {
     variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout( { children } ) {
         {
             <Providers>
                 <Header/>
+                <Navbar />
                 { children }
             </Providers>
         }
