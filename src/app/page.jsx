ebdcfd449469ee0914add115648a46992e0b24a7
Bruno Militzer <brunomilitzer.work@gmail.com>
2025-02-26 21:33:40 +0000

    Connected with TMDB Api

---
+++ src/app/page.jsx
@@ -0,0 +1,32 @@
+import Results from "@/components/results/Results";
+
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+
+export default async function Home( { searchParams } ) {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${ ACCESS_TOKEN }`,
+        }
+    };
+
+    const genre = searchParams.genre || 'fetchTrending';
+    const response = await fetch(
+        `https://api.themoviedb.org/3/${
+            genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week` }?language=en-US&page=1`, options );
+
+    const data = await response.json();
+
+    if ( !response.ok ) {
+        throw new Error( 'Failed to fetch movies.' );
+    }
+
+    const results = data.results;
+
+    return (
+        <div>
+            <Results results={results} />
+        </div>
+    )
+}
